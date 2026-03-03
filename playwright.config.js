012a10386588ba4e61b3a66ea6b262d372014824
Rahul Balashanmugam <tsca.rahul@outlook.com>
2026-03-03 12:42:20 +0000

    Commited the new

--- playwright.config.js
+++ playwright.config.js
@@ -24,11 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],
-    ['junit', { 
-      outputFile: 'test-results/results.xml',
-      suiteTitle: 'AutomationExercise Login Tests'
-    }]
+    ['junit', { outputFile: 'test-results/results.xml'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
