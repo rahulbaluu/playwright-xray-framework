b6468a8fb83196330e0dc6481e51c9d9c588fa1e
Rahul Balashanmugam <tsca.rahul@outlook.com>
2026-03-03 14:08:06 +0000

    Commited

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 test('TA-1 Verify Login Successful', async ({ page}) => {
     await page.goto('https://www.automationexercise.com/signup');
-    //await page.click('.fc-button-label');
+    await page.click('.fc-button-label');
     await page.locator('[data-qa="login-email"]').fill('rahul0987@gmail.com');
     await page.locator('[data-qa="login-password"]').fill('Rahul1234');
     await page.locator('[data-qa="login-button"]').click();
@@ -11,7 +11,7 @@ test('TA-1 Verify Login Successful', async ({ page}) => {
 
 test('TA-3 Unsuccessful Login try with wrong password', async ({ page}) => {
     await page.goto('https://www.automationexercise.com/signup');
-    //await page.click('.fc-button-label');
+    await page.click('.fc-button-label');
     await page.locator('[data-qa="login-email"]').fill('rahul0987@gmail.com');
     await page.locator('[data-qa="login-password"]').fill('Rahul1235');
     await page.locator('[data-qa="login-button"]').click();
