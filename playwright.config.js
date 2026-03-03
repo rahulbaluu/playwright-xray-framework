47036225a13646314930f0bae511772ecaa1b899
Rahul Balashanmugam <tsca.rahul@outlook.com>
2026-03-03 14:25:56 +0000

    Changes made

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,7 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -24,6 +14,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['list'],
     ['junit', { outputFile: 'results.xml'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -40,19 +31,13 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
-    },
+      name: 'chromium', use: { browserName: 'chromium' }},
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+      name: 'firefox', use: { ...devices['Desktop Firefox'] }},
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      name: 'webkit', use: { ...devices['Desktop Safari'] }},
 
     /* Test against mobile viewports. */
     // {
